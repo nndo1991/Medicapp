aa5072fe585cd3d0ffbfb127b07769029be32ed9
Fernando Garza <nndo1991@gmail.com>
2013-11-10 20:05:16 -0600

    Fixed JSON helper and QR mailer size

--- app/helpers/interiors_helper.rb
+++ app/helpers/interiors_helper.rb
@@ -8,7 +8,11 @@ module InteriorsHelper
 		    attr[:title] = event.name
 		    attr[:start] = date_with_time(event.date, event.time)
 		    attr[:end] = date_with_time(event.date, (event.time + 60*60))
-		    attr[:url] = 'http://localhost:3000/interiors/'+event.id.to_s
+			    if Rails.env.production?
+			    	attr[:url] = 'http://medicapp1.herokuapp.com/interiors/'+event.id.to_s
+			    else
+			    	attr[:url] = 'http://localhost:3000/interiors/'+event.id.to_s
+			    end
 		    attr[:allDay] = false
 		    ev << attr
 		    attr = {}
